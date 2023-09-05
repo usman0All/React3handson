c856c4941b7c4c17c547be306da49ecf0703abcf
mohd usman <usman12797@gmail.com>
2023-09-05 20:04:12 +0530

    first-Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import FormClass from './component/FormClass'
+
+function App() {
+  return (
+    <div className="App">
+    <FormClass/>
+    </div>
+  );
+}
+export default App;
